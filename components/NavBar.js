43fee238f7c7f500894ce863a0cc5d49d7a8a6fd
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2022-09-15 00:32:45 +0700

    chore: remove icons from desktop navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,6 @@
 import DarkModeButton from "./darkModeButton"
 import Link from "next/link"
-import styles from './Navbar.module.css'
+import styles from '../styles/Navbar.module.css'
 
 const NavList = ({isMobile}) => {
     const Icons = {
@@ -10,7 +10,7 @@ const NavList = ({isMobile}) => {
         linktree: (<svg className={styles.dropdownIcons} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M285.4 197.1L191.3 244.1C191.8 248 191.1 251.1 191.1 256C191.1 260 191.8 263.1 191.3 267.9L285.4 314.9C302.6 298.2 326.1 288 352 288C405 288 448 330.1 448 384C448 437 405 480 352 480C298.1 480 256 437 256 384C256 379.1 256.2 376 256.7 372.1L162.6 325.1C145.4 341.8 121.9 352 96 352C42.98 352 0 309 0 256C0 202.1 42.98 160 96 160C121.9 160 145.4 170.2 162.6 186.9L256.7 139.9C256.2 135.1 256 132 256 128C256 74.98 298.1 32 352 32C405 32 448 74.98 448 128C448 181 405 224 352 224C326.1 224 302.6 213.8 285.4 197.1L285.4 197.1z"/></svg>)
     }
 
-    return (
+    const mobileNav = (
         <>
             <li><Link href="/"><a>{Icons.home}Home</a></Link></li>
             <li><Link href="/projects"><a>{Icons.projects}Projects</a></Link></li>
@@ -18,6 +18,17 @@ const NavList = ({isMobile}) => {
             <li><a href="https://linktr.ee/guntxjakka" target="_blank" rel="noreferrer">{Icons.linktree}Linktree</a></li>
         </>
     )
+
+    const desktopNav  = (
+        <>
+            <li><Link href="/"><a>Home</a></Link></li>
+            <li><Link href="/projects"><a>Projects</a></Link></li>
+            <li><Link href="/blog"><a>Blog</a></Link></li>
+            <li><a href="https://linktr.ee/guntxjakka" target="_blank" rel="noreferrer">Linktree</a></li>
+        </>
+    )
+
+    return isMobile ? mobileNav : desktopNav
 }
 
 export default function NavBar(){
