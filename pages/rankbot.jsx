32a78f5b41e184db180478365fa14cd4773bf0bb
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2022-10-23 04:24:25 +0700

    feat: add rankbot redirect link

---
+++ pages/rankbot.jsx
@@ -0,0 +1,7 @@
+const RankbotRedir = () => {
+    return (
+        <meta httpEquiv="refresh" content="0; url=https://discord.com/api/oauth2/authorize?client_id=891682337832730694&permissions=2147511296&scope=bot%20applications.commands" />
+    )
+}
+
+export default RankbotRedir
