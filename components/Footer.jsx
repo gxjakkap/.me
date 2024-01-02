ac855a3245fd361ada89de1a3e60188c05aef608
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2024-01-02 22:13:27 +0700

    feat: add commit tag on footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,8 +7,14 @@ function getYear(){
     return (2022 !== today.getFullYear()) ? (<span> - {today.getFullYear()} </span>) : (<span> </span>)
 }
 
+function truncateGitHash(hash) {
+    return hash.substring(0, 7)
+}
 export default function Footer(){
     const [nowPlaying, setNowPlayingData] = useState(null)
+    const [currentCommit, setCurrentCommit] = useState("")
+    const [currentCommitMsg, setCurrentCommitMsg] = useState("")
+
     useEffect(() => {
         if (nowPlaying) return
         let apiOrigin = "https://guntxjakka.me"
@@ -23,11 +29,25 @@ export default function Footer(){
                 })
             })
     }, [nowPlaying])
+
+    useEffect(() => {
+        console.log(`ENV: ${process.env.NEXT_PUBLIC_VERCEL_ENV}`)
+        console.log(`SHA: ${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA}`)
+        if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview' || process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'){
+            setCurrentCommit(process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA)
+            setCurrentCommitMsg(process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_MESSAGE)
+        }
+        else {
+            setCurrentCommit('local')
+        }
+    }, [currentCommit])
+
     return (
         <footer className="footer bg-gray-800 p-10 text-slate-200">
             <div>
             <picture><img className="h-[2.6rem] w-[2.6rem]" alt="GuntxJakka's logo" src="https://res.cloudinary.com/dynrld3nm/image/upload/v1657915322/guntxjakka.me/logo_c2x50a.png"/></picture>
-                <p>© 2022{getYear()}- GuntxJakka</p>
+                <p>© 2022{getYear()} Jakkaphat Ch.</p>
+                <p>Currently {(currentCommit !== 'local') ? 'at' : 'on'} <a className="tooltip tooltip-top tooltip-primary mt-[2px] text-base hover:text-blue-500 hover:underline" data-tip={currentCommitMsg} href={(currentCommit !== 'local') ? `https://github.com/gxjakkap/.me/commit/${currentCommit}` : 'https://youtu.be/BbeeuzU5Qc8'} target="_blank" rel="noreferrer">{truncateGitHash(currentCommit)}</a></p>
             </div> 
             <div>
                 <span className="footer-title">Links</span> 
