9b3693fb3d29b259206a559776121248c337923d
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2024-10-28 01:07:29 +0700

    fix: remove metadata from redirect route handlers

--- app/(redirect)/projects/route.ts
+++ app/(redirect)/projects/route.ts
@@ -1,15 +1,6 @@
-import type { Metadata } from "next"
 import { NextRequest } from "next/server"
 
-import { metadataTemplate } from "@/lib/meta"
-
 export const dynamic = 'force-static'
-
-export const metadata: Metadata = metadataTemplate({
-    title: "projects",
-    description: "list of projects by jakka. might or might not be useful.",
-    addSiteNameInSocialTitle: true
-})
 
 export async function GET(req: NextRequest) {
     return Response.redirect(new URL(`${req.nextUrl.origin}/project`))
